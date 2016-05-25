befc1c4b1e99ed3cadce5acfc8b479fe8c404256
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2016-05-25 17:59:18 +0200

    Building form list layout
    - Update media query with include-media mixins.
    - Added form list layout styling

--- src/routes/Forms/FormsList/components/FormListView.js
+++ src/routes/Forms/FormsList/components/FormListView.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react'
+import { Table, DropdownButton, MenuItem, Pagination } from 'react-bootstrap'
 import Header from 'components/Header'
+import styles from './FormListView.scss'
 
 class FormListView extends Component {
 
@@ -7,11 +9,70 @@ class FormListView extends Component {
     super(props);
   }
 
+  get renderFormList() {
+    return (
+      <Table hover className={styles.formListTable}>
+        <thead>
+          <tr>
+            <th>My forms</th>
+            <th>Created</th>
+            <th>Completed</th>
+            <th>Viewed</th>
+            <th>Incomplete</th>
+            <th>Sent</th>
+            <th>Abandoned</th>
+            <th> - </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            [{1:1},{2:2},{3:3}].map(function(data, i) {
+              return (
+                <tr>
+                  <td>SMSF Non Coroporate Application</td>
+                  <td>01/03/16</td>
+                  <td>12</td>
+                  <td>55</td>
+                  <td>34</td>
+                  <td>133</td>
+                  <td>133</td>
+                  <td>
+                    <DropdownButton bsStyle="default" title="Quick actions" key={1} id={`dropdown-basic-${i}`}>
+                      <MenuItem eventKey="1">Action 1</MenuItem>
+                      <MenuItem eventKey="2">Action 2</MenuItem>
+                      <MenuItem eventKey="3">Action 3</MenuItem>
+                    </DropdownButton>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </Table>
+    )
+  }
+  get renderPagination() {
+    return (
+      <Pagination
+        bsSize="medium"
+        items={10}
+        activePage={1}
+        onSelect={this.handleSelect} />
+    )
+  }
+
+  handleSelect() {
+
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <h4>Form List View</h4>
+        <div className="container">
+          {this.renderFormList}
+          {this.renderPagination}
+        </div>
       </div>
     )
   }
