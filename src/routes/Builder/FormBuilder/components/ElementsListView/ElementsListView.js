e21e3212b1743e7e6e29d35a47ff37b253f603ed
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2016-07-15 05:53:56 +0200

    Remove temporary used codes.

--- src/routes/Builder/FormBuilder/components/ElementsListView/ElementsListView.js
+++ src/routes/Builder/FormBuilder/components/ElementsListView/ElementsListView.js
@@ -7,11 +7,7 @@ import {
   Panel,
   Row,
   Col,
-  Button,
-  ButtonToolbar,
-  Dropdown,
-  MenuItem,
-  Glyphicon
+  Button
 } from 'react-bootstrap';
 import _ from 'lodash';
 import questionInputs, {
@@ -87,38 +83,6 @@ class ElementsListView extends Component {
         <Accordion defaultActiveKey={0}>
           {accordionItems}
         </Accordion>
-        <ButtonToolbar>
-          <Dropdown id="dropdown-custom-1">
-            <Dropdown.Toggle>
-              <Glyphicon glyph="star" />
-              Pow! Zoom!
-            </Dropdown.Toggle>
-            <Dropdown.Menu className="super-colors">
-              <MenuItem eventKey="1">Action</MenuItem>
-              <MenuItem eventKey="2">Another action</MenuItem>
-              <MenuItem eventKey="3" active>Active Item</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="4">Separated link</MenuItem>
-            </Dropdown.Menu>
-          </Dropdown>
-
-          <Dropdown id="dropdown-custom-2">
-            <Button bsStyle="info">
-              mix it up style-wise
-            </Button>
-            <Dropdown.Toggle bsStyle="success"/>
-            <Dropdown.Menu className="super-colors">
-              <MenuItem eventKey="1">Action</MenuItem>
-              <MenuItem eventKey="2">Another action</MenuItem>
-              <MenuItem eventKey="3" active className="test">
-                <div><i>Active</i> Item</div>
-              </MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey="4">Separated link</MenuItem>
-            </Dropdown.Menu>
-          </Dropdown>
-
-        </ButtonToolbar>
       </div>
     );
   }
