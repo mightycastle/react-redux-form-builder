fd1d13c71381668b457ab325d1b6ce02562128b2
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2016-08-02 21:05:53 +1000

    Wrap the dashboard layout by authentication wrapper

--- src/routes/Dashboard/index.js
+++ src/routes/Dashboard/index.js
@@ -4,13 +4,24 @@ import FormsRoute from './Forms';
 import SubmissionsRoute from './Submissions';
 import SettingsRoute from './Settings';
 import { dashboardPath, dashboardUrl, submissionsPath } from 'helpers/urlHelper';
+import { UserAuthWrapper } from 'redux-auth-wrapper';
+import { routerActions } from 'react-router-redux'
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
 
+
+const UserIsAuthenticated = UserAuthWrapper({
+  authSelector: state => state.auth, // how to get the user state
+  redirectAction: routerActions.replace, // the redux action to dispatch for redirect
+  wrapperDisplayName: 'UserIsAuthenticated', // a nice name for this auth check
+  predicate: user => user.authStatus !== 'NOT_LOGGED_IN'
+});
+
+
 export default (store) => ({
   path: dashboardPath,
-  component: DashboardLayout,
+  component: UserIsAuthenticated(DashboardLayout),
   childRoutes: [
     FormsRoute(store),
     SubmissionsRoute(store),
