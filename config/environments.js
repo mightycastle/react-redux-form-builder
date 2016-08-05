926763f86166cd4da375550926c5f3a6e31ca66e
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2016-08-05 15:57:25 +1000

    Updated production settings

--- config/environments.js
+++ config/environments.js
@@ -41,8 +41,8 @@ export default {
     },
     globals: {
       ...config.globals,
-      API_URL: JSON.stringify('https://api.emondo.com.au'),
-      FRONTEND_ROOT: JSON.stringify('http://new.emondo.com.au'),
+      API_URL: JSON.stringify('http://api.emondo.co'),
+      FRONTEND_ROOT: JSON.stringify('http://app.emondo.co'),
       GOOGLE_MAP_API_KEY: JSON.stringify('AIzaSyCtwNDvDLxw-JSB8RBvtdMuW2Qjh5AypEk')
     }
   })
