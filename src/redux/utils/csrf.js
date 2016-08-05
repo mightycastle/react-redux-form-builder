331dd0a1052f9a6c9c1171ab43f1d9ec1e3b892b
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2016-08-05 11:52:25 +0200

    Update getCsrfToken function.

--- src/redux/utils/csrf.js
+++ src/redux/utils/csrf.js
@@ -1,24 +1,5 @@
+import cookie from 'react-cookie';
 
-var getCookie = function (name) {
-  var cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i].trim();
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === (name + '=')) {
-        cookieValue = decodeURIComponent(
-          cookie.substring(name.length + 1)
-        );
-        break;
-      }
-    }
-  }
-  return cookieValue;
-};
-
-var getCsrfToken = function() {
-  return getCookie('csrftoken');
-};
+const getCsrfToken = () => cookie.load('csrftoken');
 
 export default getCsrfToken;
