3cc89694e28014a96d80ab143d012747e9485b30
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2016-08-17 16:32:51 +0200

    Implement styling for AnswerOutputArea in form builder.

--- src/components/QuestionEditFields/AnswerOutputArea/AnswerOutputArea.js
+++ src/components/QuestionEditFields/AnswerOutputArea/AnswerOutputArea.js
@@ -4,12 +4,22 @@ import React, {
 } from 'react';
 import {
   Button,
-  Col
+  FormGroup,
+  InputGroup,
+  FormControl,
+  OverlayTrigger,
+  Popover
 } from 'react-bootstrap';
-import { FaTrash } from 'react-icons/lib/fa';
+import Switch from 'rc-switch';
+import popoverTexts from 'schemas/popoverTexts';
+import {
+  MdCropFree,
+  MdDelete
+} from 'react-icons/lib/md';
 import EditSection from '../EditSection/EditSection';
-import EditRow from '../EditRow/EditRow';
 import SectionTitle from '../SectionTitle/SectionTitle';
+import _ from 'lodash';
+import styles from './AnswerOutputArea.scss';
 
 class AnswerOutputArea extends Component {
   static propTypes = {
@@ -21,23 +31,81 @@ class AnswerOutputArea extends Component {
     resetMappingInfo();
   }
 
+  getPopover(popoverId) {
+    return (
+      <Popover id={`${popoverId}Popover`}>
+        {popoverTexts[popoverId]}
+      </Popover>
+    );
+  }
+
+  getLabelByIndex(index) {
+    return String.fromCharCode('A'.charCodeAt(0) + index);
+  }
+
+  renderList() {
+    return _.map([1, 2, 3], (item, index) => {
+      return (
+        <FormGroup key={index} className={styles.formGroup}>
+          <InputGroup>
+            <InputGroup.Addon className={styles.itemLabel}>
+              {this.getLabelByIndex(index)}
+            </InputGroup.Addon>
+            <FormControl type="text" />
+          </InputGroup>
+          <ul className={styles.actionItems}>
+            <li>
+              <OverlayTrigger trigger="hover" overlay={this.getPopover('reselectOutputArea')}>
+                <Button className={styles.actionButton} onClick={this.handleDeleteSelection}>
+                  <MdCropFree size={18} />
+                </Button>
+              </OverlayTrigger>
+            </li>
+            <li>
+              <Button className={`${styles.actionButton} ${styles.deleteButton}`}
+                onClick={this.handleDeleteSelection}
+              >
+                <span className={styles.removeLabel}>Remove?</span>
+                <MdDelete size={18} />
+              </Button>
+            </li>
+          </ul>
+        </FormGroup>
+      );
+    });
+  }
+
+  renderAddButton() {
+    return (
+      <Button block className={styles.addButton}>
+        + Add new answer output area
+      </Button>
+    );
+  }
+
+  renderAllowOtherOption() {
+    return (
+      <div className={styles.otherOption}>
+        <div className={styles.otherOptionLeft}>
+          <SectionTitle title={'Allow "Other" option'} />
+        </div>
+        <div className={styles.otherOptionRight}>
+          <Switch />
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <EditSection>
-        <EditRow>
-          <Col xs={6}>
-            <SectionTitle
-              title="Answer output area(s)"
-              popoverId="outputArea"
-              description="(Leave empty if not required)"
-            />
-          </Col>
-          <Col xs={6} className="text-right">
-            <Button bsSize="small" onClick={this.handleDeleteSelection}>
-              <FaTrash />
-            </Button>
-          </Col>
-        </EditRow>
+        <SectionTitle
+          title="Answer output area(s)"
+          popoverId="outputArea"
+        />
+        {this.renderList()}
+        {this.renderAddButton()}
+        {this.renderAllowOtherOption()}
       </EditSection>
     );
   }
--- src/schemas/popoverTexts.js
+++ src/schemas/popoverTexts.js
@@ -3,6 +3,7 @@ const popoverTexts = {
   validationMinLength: 'TODO: Add question validation min length popover text here.',
   validationMaxLength: 'TODO: Add question validation max length popover text here.',
   outputArea: 'TODO: Add output area popover text here.',
+  reselectOutputArea: 'Reselect output area',
   isRequired: 'TODO: Add isRequired popover text here.'
 };
 
