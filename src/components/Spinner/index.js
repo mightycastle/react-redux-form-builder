32794cb573a2389c9552f8e6df0c2e166ef82af1
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2016-08-19 17:20:27 +1000

    Update the spinner component

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import styles from './Spinner.scss';
 
 class Spinner extends Component {
+
+  static propTypes = {
+    foreground: PropTypes.string,
+    background: PropTypes.string
+  }
+
+  static defaultProps = {
+    foreground: 'blue',
+    background: 'white'
+  };
+
+  /*
+  filenames for spinners are in the format emondo_spinner_[foreground]_[background].gif
+  */
+
   render() {
-    var spinnerPath = require('./emondo_spinner_darkgrey.gif');
+    var colours = this.props.foreground + '_' + this.props.background;
+    var spinnerPath = require('./emondo_spinner_' + colours + '.gif');
     return (
       <img src={spinnerPath} alt="Loading" className={styles.spinner} />
     );
