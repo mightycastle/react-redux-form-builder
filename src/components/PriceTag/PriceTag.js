8dde2daf731e032ab4434b9afdd22c181bf7c024
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2016-08-22 15:22:52 +1000

    Add component PriceTag

---
+++ src/components/PriceTag/PriceTag.js
@@ -0,0 +1,36 @@
+import React, {
+  Component,
+  PropTypes
+} from 'react';
+
+export default class PriceTag extends Component {
+  static propTypes = {
+    price: PropTypes.number.isRequired,
+    currency: PropTypes.string
+  }
+  processPrice = () => {
+    const { price } = this.props;
+    if (price < 0) {
+      const value = Math.abs(price);
+      return {
+        mark: '- ',
+        value: value % 100 === 0 ? value / 100 : value / 100 + '.' + value % 100
+      };
+    } else {
+      return {
+        mark: null,
+        value: price % 100 === 0 ? price / 100 : price / 100 + '.' + price % 100
+      };
+    }
+  }
+  render() {
+    return (
+      <span>
+        {this.processPrice().mark}
+        {this.props.currency || ''}
+        {' $'}
+        {this.processPrice().value}
+      </span>
+    );
+  }
+};
