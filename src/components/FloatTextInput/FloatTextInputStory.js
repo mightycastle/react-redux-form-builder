77420527b3624d0d7d7a9235a5324d69f0d796ea
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-06-27 12:28:33 +0900

    Fixed problems

--- src/components/FloatTextInput/FloatTextInputStory.js
+++ src/components/FloatTextInput/FloatTextInputStory.js
@@ -4,14 +4,23 @@ import FloatTextInput from './FloatTextInput';
 import styles from './FloatTextInputStory.scss';
 import classNames from 'classnames/bind';
 
-const cx = classNames.bind(styles);
+
 storiesOf('Input', module)
-  .add('Text Input', () => (
-    <div>
-      <FloatTextInput id="default" placeholder="Deault state" primaryColour={'#3993d1'} className={cx('default')} />
-      <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Filled state'} className={cx('default')} />
-      <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Active state'} autoFocus className={cx('default')} />
-      <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Error state'} error={true} errorMessage={'Error message here'} className={cx('default')} />
-    </div>
-  )
+  .add('Text Input', () => {
+    const cx = classNames.bind(styles);
+    let isError = true;
+    let onBlur = (event) => {
+      console.log(event.target.value);
+      isError = true;
+    }
+    return (
+      <div>
+        <FloatTextInput name="default" placeholder="Deault state" primaryColour={'#3893d0'} className={cx('default')} />
+        <FloatTextInput name="filled" placeholder="Label" primaryColour={'#3893d0'} value={'Filled state'} className={cx('default')} />
+        <FloatTextInput name="active" placeholder="Label" primaryColour={'#3893d0'} value={'Active state'} autoFocus className={cx('default')} />
+        <FloatTextInput name="error" placeholder="Label" primaryColour={'#3893d0'} className={cx('default')}
+          value={'Error state'} isError={isError} onBlur={onBlur} errorMessage={'Error message here'} />
+      </div>
+    );
+  }
 );
