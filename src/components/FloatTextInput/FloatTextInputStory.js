5744c1eace18385b1e41565399eb11876a719889
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-06-27 10:35:50 +0900

    Add testing inputs at story board

--- src/components/FloatTextInput/FloatTextInputStory.js
+++ src/components/FloatTextInput/FloatTextInputStory.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import FloatTextInput from './FloatTextInput';
+import styles from './FloatTextInputStory.scss';
+import classNames from 'classnames/bind';
 
+const cx = classNames.bind(styles);
 storiesOf('Input', module)
   .add('Text Input', () => (
-    <div style={{fontSize: '50px'}}>
-      <FloatTextInput id="test" placeholder="state" primaryColour={'#3993d1'} error={true} errorMessage={'test'} />
+    <div>
+      <div className={cx('default')}>
+        <FloatTextInput id="default" placeholder="Deault state" primaryColour={'#3993d1'} />
+      </div>
+      <div className={cx('default')}>
+        <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Filled state'} />
+      </div>
+      <div className={cx('default')}>
+        <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Active state'} autoFocus />
+      </div>
+      <div className={cx('default')}>
+        <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Error state'} error={true} errorMessage={'Error message here'} />
+      </div>
     </div>
+
   ));
