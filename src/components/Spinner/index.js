e467fdfb57bdcbdd617b8d10e331b6ad22d216be
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-06-27 14:50:35 +0900

    Fixed classname bind in Spinner

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import styles from './Spinner.scss';
+import classNames from 'classnames/bind';
 
 class Spinner extends Component {
   render() {
     var spinnerPath = require('./emondo_spinner_darkgrey.gif');
     return (
-      <img src={spinnerPath} alt="Loading" className={styles.spinner} />
+      <img src={spinnerPath} alt="Loading" className={classNames.bind(styles)('spinner')} />
     );
   }
 }
