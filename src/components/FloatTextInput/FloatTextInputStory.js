55002fd6ccb1048778f2f3f392650da6b0d056ba
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-06-27 10:41:09 +0900

    Add testing for combined classname

--- src/components/FloatTextInput/FloatTextInputStory.js
+++ src/components/FloatTextInput/FloatTextInputStory.js
@@ -8,18 +8,10 @@ const cx = classNames.bind(styles);
 storiesOf('Input', module)
   .add('Text Input', () => (
     <div>
-      <div className={cx('default')}>
-        <FloatTextInput id="default" placeholder="Deault state" primaryColour={'#3993d1'} />
-      </div>
-      <div className={cx('default')}>
-        <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Filled state'} />
-      </div>
-      <div className={cx('default')}>
-        <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Active state'} autoFocus />
-      </div>
-      <div className={cx('default')}>
-        <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Error state'} error={true} errorMessage={'Error message here'} />
-      </div>
+      <FloatTextInput id="default" placeholder="Deault state" primaryColour={'#3993d1'} className={cx('default')} />
+      <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Filled state'} className={cx('default')} />
+      <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Active state'} autoFocus className={cx('default')} />
+      <FloatTextInput id="test" placeholder="Label" primaryColour={'#3993d1'} value={'Error state'} error={true} errorMessage={'Error message here'} className={cx('default')} />
     </div>
-
-  ));
+  )
+);
