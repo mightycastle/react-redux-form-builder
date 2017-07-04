3f03868f566c5925456a9e7b75b5327c378dd602
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-07-04 12:38:59 +0900

    Update AlertMessage include child component

--- src/components/AlertMessage/AlertMessage.js
+++ src/components/AlertMessage/AlertMessage.js
@@ -6,19 +6,39 @@ import styles from './AlertMessage.scss';
 import classNames from 'classnames/bind';
 
 class AlertMessage extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    list: PropTypes.array
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+  handleClick = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
-    const {list} = this.props;
+    const {children, list} = this.props;
     var cx = classNames.bind(styles);
     return (
-      <div className={cx('alertWrapper')}>
-        <ul className={cx('messageList')}>
-          {list.map((alert, index) => (
-            <li key={index} className={cx('messageItem')}>
-              <div className={cx('messageContent', {'unreadMessage': !alert.isRead})}>{alert.message}</div>
-              <div className={cx('messageTime')}>{alert.time}</div>
-            </li>
-          ))}
-        </ul>
+      <div className={cx('alertMessageSection')} onClick={this.handleClick}>
+        {children}
+        <div className={cx('alertBox', {hide: !this.state.isOpen})}>
+          <div className={cx('alertWrapper')}>
+            <ul className={cx('messageList')}>
+              {list.map((alert, index) => (
+                <li key={index} className={cx('messageItem')}>
+                  <div className={cx('messageContent', {'highlighted': alert.highlight})}>{alert.message}</div>
+                  <div className={cx('messageTime')}>{alert.time}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/AlertMessage/AlertMessgeStory.js
+++ src/components/AlertMessage/AlertMessgeStory.js
@@ -1,43 +1,47 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import AlertMessage from './AlertMessage';
-
+import { FaBell } from 'react-icons/lib/fa';
 storiesOf('Dropdown', module)
   .add('Alert Message', () => {
     const list = [{
-      isRead: false,
+      highlight: true,
       message: 'Jordan McCown completed the SMSF tlksdfn sdkfsdfsdfsdfsdfsdf dsf',
       time: '2m ago',
       link: '/forms/3'
     }, {
-      isRead: true,
+      highlight: false,
       message: 'Lihan Li viewed the Personal Form',
       time: '5m ago',
       link: '/forms/3'
     }, {
-      isRead: true,
+      highlight: false,
       message: 'Shaun Harvey abandonded the Some thing',
       time: '1h ago',
       link: '/forms/3'
     }, {
-      isRead: true,
+      highlight: false,
       message: 'Jordan McCown completed the SMSF tlksdfn sdkf',
       time: '2h ago',
       link: '/forms/3'
     }, {
-      isRead: true,
+      highlight: false,
       message: 'Jordan McCown completed the SMSF tlksdfn sdkf',
       time: '2h ago',
       link: '/forms/3'
     }, {
-      isRead: true,
+      highlight: false,
       message: 'Jordan McCown completed the SMSF tlksdfn sdkf',
       time: '2h ago',
       link: '/forms/3'
     }];
     return (
-      <div>
-        <AlertMessage list={list} />
+      <div style={{position: 'absolute', top: '100px', left: '300px'}}>
+        <AlertMessage list={list}>
+          <span style={{border: '1px solid #333'}}>
+            <FaBell />3
+          </span>
+        </AlertMessage>
       </div>
     );
   }
