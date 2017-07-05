dd038b8ef860980bbad648b2824ee98de963694a
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-07-05 10:49:28 +0900

    Setting default value for DocumentType

--- src/containers/IDVerificationFormContainer.js
+++ src/containers/IDVerificationFormContainer.js
@@ -1,8 +1,12 @@
 import { reduxForm } from 'redux-form';
 import IDVerificationForm from 'components/IDVerification/IDVerificationForm';
-import idVerificationFormSchema from 'schemas/idVerificationFormSchema';
+import idVerificationFormSchema, {
+  identityConstants } from 'schemas/idVerificationFormSchema';
 
 export default reduxForm({
   form: 'IDVerificationForm',
+  initialValues: {
+    'type': identityConstants.DVSPASSPORT
+  },
   ...idVerificationFormSchema
 })(IDVerificationForm);
