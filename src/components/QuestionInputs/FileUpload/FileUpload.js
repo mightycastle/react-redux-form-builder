7c26a5ab4705621391cf53ee1c2e271494a43e09
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-07-18 12:13:29 +0900

    Submit field value to backend

--- src/components/QuestionInputs/FileUpload/FileUpload.js
+++ src/components/QuestionInputs/FileUpload/FileUpload.js
@@ -18,6 +18,7 @@ const XHR_SUCCESS = 'XHR_SUCCESS';
 const XHR_UPLOADING = 'XHR_UPLOADING';
 const XHR_WAITING = 'XHR_WAITING';
 const XHR_FAIL = 'XHR_FAIL';
+const REMOVED = 'REMOVED';
 
 class FileUpload extends Component {
 
@@ -26,42 +27,73 @@ class FileUpload extends Component {
   };
 
   static propTypes = {
+    /*
+    value: array
+    [{file_name: 'sheep.png', attachment_id: 1, file_size: 10000}]
+    */
+    value: PropTypes.array,
+
+    /*
+    file types to accept
+    */
     accept: PropTypes.string,
     maxNumberOfFiles: PropTypes.number,
     maxTotalFileSizeKb: PropTypes.number,
     onChange: PropTypes.func,
-    onEnterKey: PropTypes.func
+    onEnterKey: PropTypes.func,
+    formId: PropTypes.number,
+    sessionId: PropTypes.number
   };
 
   static defaultProps = {
+    value: [],
     accept: '*',
     maxNumberOfFiles: 3,
-    maxTotalFileSizeKb: 100000
+    maxTotalFileSizeKb: 10000
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-        {file: {name: 'test1'}, progress: 100, status: 'XHR_SUCCESS'},
-        {file: {name: 'test2'}, progress: 60, status: 'XHR_UPLOADING'}
-      ]
+      items: this.props.value
     };
-    this.xhr = null;
+    // this.xhr = null;
   }
 
-  hasMaxFiles = () => {
+  hasMaxFiles = (additionalFileSize = 0) => {
+    /*
+    argument additionalFileSize is used when a file is selected for upload
+    to check if it will go over the max total file size
+    */
     const { items } = this.state;
-    // count items that haven't failed
-    // TODO: don't count cancelled/removed items
-    var numFiles = _.reject(items, function (el) {
-      return (el.status === XHR_FAIL);
-    }).length;
-    if (numFiles >= this.props.maxNumberOfFiles) {
+    // count items ((has attachment_id && status not REMOVED) or status not XHR_FAIL)
+    var numFiles = 0;
+    var totalFileSize = additionalFileSize;
+    _.forEach(items, function (value) {
+      if((value.attachment_id && value.status !== REMOVED) || value.status !== XHR_FAIL) {
+        numFiles += 1;
+        var s = value.file_size || value.file.size;
+        totalFileSize += s;
+      }
+    });
+    if (numFiles >= this.props.maxNumberOfFiles ||
+      (totalFileSize / 1024) >= this.props.maxTotalFileSizeKb) {
       return true;
     }
-    // TODO: test total file size
     return false;
+  }
+
+  handleChange = () => {
+    var values = [];
+    _.forEach(this.state.items, function (value) {
+      if (value.status !== REMOVED && value.attachment_id) {
+        var s = value.file_size || value.file.size;
+        var x = {file_name: value.file_name, file_size: s, attachment_id: value.attachment_id};
+        values.push(x);
+      }
+    });
+    // console.log(values);
+    this.props.onChange(values);
   }
 
   handleKeyDown = (event) => {
@@ -77,12 +109,16 @@ class FileUpload extends Component {
   }
 
   handleFileSelect = (event) => {
-    // const item = {this.refs.fileInput.files[0], progress: 0, status: XHR_INIT};
     const item = this.fileToItem(this.refs.fileInput.files[0]);
-    var newItems = this.state.items;
-    var newIndex = newItems.push(item) - 1;
-    this.setState({ items: newItems });
-    this.upload(newIndex);
+    // check file size
+    if (this.hasMaxFiles(item.file.size)) {
+      return false;
+    } else {
+      var newItems = this.state.items;
+      var newIndex = newItems.push(item) - 1;
+      this.setState({ items: newItems });
+      this.upload(newIndex);
+    }
   }
 
   fileToItem(file) {
@@ -91,7 +127,7 @@ class FileUpload extends Component {
 
   upload(index) {
     const item = this.state.items[index];
-    var requestURL = `${API_URL}/form_document/api/form_response/attachment/`;
+    var requestURL = `${API_URL}/form_document/api/form_response/upload_attachment/`;
     var method = 'POST';
     if (item) {
       const file = item.file;
@@ -99,7 +135,10 @@ class FileUpload extends Component {
       const xhr = new XMLHttpRequest();
       xhr.withCredentials = true;
       formData.append('file', file, file.name);
-
+      formData.append('form_id', this.props.formId);
+      if (typeof (this.props.sessionId) !== 'undefined' && !isNaN(this.props.sessionId)) {
+        formData.append('response_id', this.props.sessionId);
+      }
       xhr.onload = this.handleUploadResponse;
 
       xhr.upload.onprogress = (event) => {
@@ -113,7 +152,7 @@ class FileUpload extends Component {
       xhr.setRequestHeader('X-CSRFToken', getCsrfToken());
       xhr.setRequestHeader('Accept', 'application/json');
       xhr.send(formData);
-      this.xhr = xhr;
+      // this.xhr = xhr;
       const newItems = [...this.state.items];
       newItems[index] = Object.assign({}, this.state.items[index], {
         status: XHR_UPLOADING
@@ -142,24 +181,32 @@ class FileUpload extends Component {
     var index = this.state.items.length - 1;
     const newItems = [...this.state.items];
     if (_.inRange(event.target.status, 200, 206)) {
-      console.log(event.target.response);
-      // TODO: add id, filename, url from response to items[index]
+      var response = JSON.parse(event.target.response);
       newItems[index] = Object.assign({}, this.state.items[index], {
-        status: XHR_SUCCESS
+        status: XHR_SUCCESS,
+        file_name: response.file_name,
+        attachment_id: response.attachment_id
       });
-      this.setState({items: newItems});
+      this.setState({items: newItems}, function () {
+        this.handleChange();
+      });
     } else {
       newItems[index] = Object.assign({}, this.state.items[index], {
         status: XHR_FAIL
       });
       this.setState({items: newItems});
-      console.log(event.target.response);
       // onFail(JSON.parse(event.target.response));
     }
   }
 
-  cancelFile() {
-    console.log('TODO: delete function');
+  removeFile(index) {
+    const newItems = [...this.state.items];
+    newItems[index] = Object.assign({}, this.state.items[index], {
+      status: REMOVED
+    });
+    this.setState({items: newItems}, function () {
+      this.handleChange();
+    });
   }
 
   getFileWrapperClass(status) {
@@ -168,7 +215,8 @@ class FileUpload extends Component {
       [styles.success]: status === XHR_SUCCESS,
       [styles.fail]: status === XHR_FAIL,
       [styles.uploading]: status === XHR_UPLOADING,
-      [styles.waiting]: status === XHR_WAITING
+      [styles.waiting]: status === XHR_WAITING,
+      [styles.removed]: status === REMOVED
     });
   }
 
@@ -181,18 +229,24 @@ class FileUpload extends Component {
       <div className={styles.fileset}>
         {items.map((item, index) => {
           const file = item.file;
-          const fileSize = fileSizeWithUnit(file.size);
-          const fileSizeUploaded = fileSizeWithUnit(file.size * item.progress / 100);
+          var fileSize, fileSizeUploaded;
+          if (item.file_size) {
+            // for files that are already uploaded and came from this.props.value
+            fileSize = fileSizeWithUnit(item.file_size);
+          } else {
+            fileSize = fileSizeWithUnit(file.size);
+            fileSizeUploaded = fileSizeWithUnit(file.size * item.progress / 100);
+          }
           return (
             <div key={index} className={this.getFileWrapperClass(item.status)}>
               <div className={styles.fileTopSection}>
                 <span className={styles.fileDetails}>
-                  <span className={styles.fileName}>{file.name}</span>
+                  <span className={styles.fileName}>{item.file_name || file.name}</span>
                   <span className={styles.fileSize}>{fileSize}</span>
                 </span>
-                {item.status === XHR_SUCCESS &&
+                {item.attachment_id &&
                   <a className={styles.fileStatus} href="javascript:;"
-                    onClick={function () { that.cancelFile(index); }}>
+                    onClick={function () { that.removeFile(index); }}>
                     <FaClose style={{verticalAlign: 'text-bottom'}} />
                   </a>
                 }
