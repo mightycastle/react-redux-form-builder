6dce7a75e448f9b9cee590fee76ed7de6b76bc68
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-07-31 16:26:52 +0900

    constants for formBuilderBoxMappingType

--- src/constants/formBuilder/index.js
+++ src/constants/formBuilder/index.js
@@ -4,3 +4,8 @@ export const formBuilderSelectMode = {
   QUESTION_BOX_MAPPING_VIEW: 1,
   QUESTION_DETAIL_VIEW: 2
 };
+
+export const formBuilderBoxMappingType = {
+  STANDARD: 'STANDARD',
+  BLOCK: 'BLOCK'
+};
