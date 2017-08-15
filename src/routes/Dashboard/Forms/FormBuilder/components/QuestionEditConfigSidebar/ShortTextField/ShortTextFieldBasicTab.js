9fb697ed27dceeb75d945882a9774c576ae264fd
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-08-15 09:15:34 +0900

    Added AnswerOutputTypeStatus indicator

--- src/routes/Dashboard/Forms/FormBuilder/components/QuestionEditConfigSidebar/ShortTextField/ShortTextFieldBasicTab.js
+++ src/routes/Dashboard/Forms/FormBuilder/components/QuestionEditConfigSidebar/ShortTextField/ShortTextFieldBasicTab.js
@@ -3,9 +3,11 @@ import React, {
   PropTypes
 } from 'react';
 import InstructionDescription from 'components/QuestionEditFields/InstructionDescription';
+import AnswerOutputTypeStatus from 'components/QuestionEditFields/AnswerOutputTypeStatus';
 import LengthValidation from 'components/QuestionEditFields/LengthValidation';
 import RangeValidation from 'components/QuestionEditFields/RangeValidation';
 import RequiredValidation from 'components/QuestionEditFields/RequiredValidation';
+import EditSection from 'components/QuestionEditFields/EditSection';
 
 class ShortTextFieldBasicTab extends Component {
   static propTypes = {
@@ -16,6 +18,10 @@ class ShortTextFieldBasicTab extends Component {
   render() {
     return (<div>
       <InstructionDescription {...this.props} />
+      <EditSection>
+        <AnswerOutputTypeStatus
+          status={this.props.currentElement.defaultMappingType} />
+      </EditSection>
       <LengthValidation {...this.props} />
       <RangeValidation {...this.props} />
       <RequiredValidation {...this.props} />
