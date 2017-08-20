01878fbf2493ca246ecb8cd97905d52f8d8f4e39
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-08-20 09:16:06 +0900

    Remove unused ``action``

--- src/components/QuestionInputs/Statement/StatementStory.js
+++ src/components/QuestionInputs/Statement/StatementStory.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
 import Statement from './Statement';
 
 storiesOf('Statement', module)
