76242d22d2358f436b50e1fa0cf9da6b2a54bf10
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-08-20 14:05:25 +0900

    Remove story unused action function

--- src/components/TextInput/TextInputStory.js
+++ src/components/TextInput/TextInputStory.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
 import TextInput from './TextInput';
 
 storiesOf('Text Input', module)
