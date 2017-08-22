4960e9f21e922bf360ab6061c237a967559c5d23
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-08-22 19:38:00 +0900

    Added origin of regex

--- src/helpers/validationHelper.js
+++ src/helpers/validationHelper.js
@@ -4,6 +4,7 @@ export const validateIsRequired = (value) => {
 };
 
 export const validateIsEmail = (value) => {
+  // from http://emailregex.com/
   var re = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i; // eslint-disable-line
   return re.test(value);
 };
