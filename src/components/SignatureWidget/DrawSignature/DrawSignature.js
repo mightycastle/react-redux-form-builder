42039e9ca02aa130f7a121f7e370c4421b0cfa61
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-08-29 09:31:01 +0900

    Change existing draw signature with new colour selected

--- src/components/SignatureWidget/DrawSignature/DrawSignature.js
+++ src/components/SignatureWidget/DrawSignature/DrawSignature.js
@@ -9,6 +9,18 @@ import styles from './DrawSignature.scss';
 import { IoIosRefreshEmpty } from 'react-icons/lib/io';
 import SignatureTabs from '../SignatureTabs';
 
+/*
+ * Convert Hex to RGB refer to: http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+ **/
+const hexToRgb = (hex) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+};
+
 class DrawSignature extends Component {
 
   static propTypes = {
@@ -42,11 +54,31 @@ class DrawSignature extends Component {
   }
 
   handleColourChange = (colour) => {
-    this.setState({
-      drawSignatureColour: colour
-    });
-    this.refs.signatureCanvas.penColor = colour;
-    this.handleClear();
+    const oldColour = this.state.drawSignatureColour;
+    if (oldColour !== colour) {
+      this.setState({
+        drawSignatureColour: colour
+      });
+      this.refs.signatureCanvas.penColor = colour;
+      /*
+       * Replace canvas pixel colour with new colour refer to: http://jsfiddle.net/m1erickson/4apAS/
+       * Change original pixal match from colour match to background match due to anti aliasing colour mismatch.
+       **/
+      const canvas = this.refs.signatureCanvas.refs.cv;
+      const ctx = canvas.getContext('2d');
+      let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const newColourRGB = hexToRgb(colour);
+      for (let i = 0; i < imageData.data.length; i+=4) {
+        if (imageData.data[i] !== 255 &&
+          imageData.data[i+1] !== 255 &&
+          imageData.data[i+2] !== 255) {
+          imageData.data[i] = newColourRGB.r;
+          imageData.data[i+1] = newColourRGB.g;
+          imageData.data[i+2] = newColourRGB.b;
+        }
+      }
+      ctx.putImageData(imageData, 0, 0);
+    }
   }
 
   handleClear = () => {
