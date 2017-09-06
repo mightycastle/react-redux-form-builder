f053658a424700538adc544649e10732531d3399
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-09-06 12:26:33 +0900

    Add extraClass support at PlainHeader

--- src/components/Headers/PlainHeader/PlainHeader.js
+++ src/components/Headers/PlainHeader/PlainHeader.js
@@ -1,15 +1,22 @@
 import React,
 {
-  Component
+  Component,
+  PropTypes
 } from 'react';
 import StackLogo from 'components/Logos/StackLogo';
 import styles from './PlainHeader.scss';
 import { Grid } from 'react-bootstrap';
+import classNames from 'classnames';
 
 class PlainHeader extends Component {
+
+  static propTypes = {
+    extraClass: PropTypes.string
+  }
+
   render() {
     return (
-      <div className={styles.header}>
+      <div className={classNames(styles.header, this.props.extraClass)}>
         <Grid fluid>
           <div className="text-center">
             <StackLogo className={styles.logo} width="auto" height={38} logoStyle="white" />
