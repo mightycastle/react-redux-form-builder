11a308a2d363e5b402584c7acbdf6a597bbe8577
Sousuke Tanaka <tanaka.sousuke419@gmail.com>
2017-09-13 05:56:28 +0900

    Update the SubmissionsFilter component and make the button order consistent with the forms tab

--- src/routes/Dashboard/Submissions/SubmissionsList/components/SubmissionsFilter/SubmissionsFilter.js
+++ src/routes/Dashboard/Submissions/SubmissionsList/components/SubmissionsFilter/SubmissionsFilter.js
@@ -2,19 +2,22 @@ import React, {
   Component,
   PropTypes
 } from 'react';
-import Button from 'components/Buttons/DashboardButtons/Button';
 import SelectButton from 'components/Buttons/SelectButton';
 import { ButtonToolbar } from 'react-bootstrap';
-import { IoStatsBars, IoRefresh } from 'react-icons/lib/io';
+import Icon from 'components/Icon';
 import styles from './SubmissionsFilter.scss';
 
 class SubmissionsFilter extends Component {
 
   static propTypes = {
+    refresh: PropTypes.func,
     pageSize: PropTypes.number,
     setPageSize: PropTypes.func,
-    formAction: PropTypes.func,
     selectedItems: PropTypes.array
+  }
+
+  handleRefresh = () => {
+    this.props.refresh();
   }
 
   get timeOptions() {
@@ -46,44 +49,29 @@ class SubmissionsFilter extends Component {
       }
     ];
   }
-  get actionOptions() {
-    const number = this.props.selectedItems.length;
-    return [
-      {
-        key: 'edit',
-        label: 'Edit',
-        isDisabled: number !== 1
-      },
-      {
-        key: 'delete',
-        label: 'Delete',
-        isDisabled: number === 0
-      }
-    ];
-  }
+
   render() {
-    const { pageSize, setPageSize, formAction } = this.props;
-    const iconStyle = {verticalAlign: 'text-bottom', marginRight: '6px'};
+    const { pageSize, setPageSize } = this.props;
     return (
       <div className={styles.filterContainer}>
         <ButtonToolbar className="pull-left">
-          <Button style="formButton">
-            <IoRefresh size={18} style={iconStyle} />
-            {' '}
-            Refresh
-          </Button>
-          <Button style="formButton">
-            <IoStatsBars size={18} style={iconStyle} />
-            {' '}
-            Customise
-          </Button>
+          <SelectButton
+            className={styles.formButton}
+            label={
+              <strong>
+                <Icon name="Refresh" height={37} width={15} className={styles.buttonIcon} />
+                {' '}
+                Refresh
+              </strong>}
+            isStaticValue
+            onClick={this.handleRefresh} />
+          <SelectButton className={styles.formButton}
+            optionsList={this.paginationOptions}
+            label="Show"
+            value={pageSize}
+            onChange={setPageSize} />
         </ButtonToolbar>
         <ButtonToolbar className="pull-right">
-          <SelectButton className={styles.formButton} optionsList={this.timeOptions} value="This Week" />
-          <SelectButton className={styles.formButton} optionsList={this.paginationOptions} label="Show"
-            value={pageSize} onChange={setPageSize} />
-          <SelectButton className={styles.formButton} optionsList={this.actionOptions} value="Actions" isStaticValue
-            onChange={formAction} />
         </ButtonToolbar>
         <div className="clearfix"></div>
       </div>
